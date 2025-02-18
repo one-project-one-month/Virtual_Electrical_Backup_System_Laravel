d2da2f55952e79ec390f92e5b67c29340a33b8fc
YuzanaKyaw <yuzanakyaw160@gmail.com>
2025-02-18 19:56:42 +0630

    [fix] api route error

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Api\Test\TestCategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api/v1')->middleware(['auth:sanctum'])->group(function () {
+Route::prefix('v1')->middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('test-categories', TestCategoryController::class);
 });
