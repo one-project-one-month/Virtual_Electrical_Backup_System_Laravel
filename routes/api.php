ed855a136ac97b618688c7f0319bf6c63d9ac739
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-02-20 14:50:15 +0630

    Battery Types CRUD API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\BatteryTypeController;
 use App\Http\Controllers\Api\Test\TestCategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('test-categories', TestCategoryController::class);
 });
+
+Route::apiResource('/battery_types', BatteryTypeController::class);
