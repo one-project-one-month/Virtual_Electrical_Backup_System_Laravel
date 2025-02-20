03c9389b52404ab7f57192e561fede86394b154c
waiyanpaing23 <waiyanpaing2304@gmail.com>
2025-02-20 14:34:38 +0630

    [add] add generator files

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\Test\TestCategoryController;
+use App\Http\Controllers\GeneratorController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('test-categories', TestCategoryController::class);
+    
+    Route::apiResource('generator', GeneratorController::class);
 });
